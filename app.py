53181a5c99eeb5d993bdad0125d7984043ef5ba7
יועד רביטל <yoad.ravitel@gmail.com>
2023-09-21 22:12:07 +0300

    update

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-import flask
-import os
+from flask import *
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+app.secret_key = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
 app.static_folder = 'static'
 
 @app.route("/")
 def home():
-    return flask.render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/select/")
 def select():
-    return flask.render_template("select.html")
+    return render_template("select.html")
 
-@app.route("/search/")
+@app.route("/search/", methods=["POST", "GET"])
 def search():
-    
-    q = flask.request.args.get('q')
-    league_id = flask.request.args.get('id')
-    if q: print(q)
-    if r: print(league_id)
-    
-    return flask.render_template("search.html")
+    if request.method == "POST":
+        queue = request.form['nm']
+        return redirect(url_for("psearch", id=queue))
+    else:
+        return render_template("search.html")    
+
+@app.route("/player-search?uid=<id>") 
+def psearch(id):
+    return render_template("player.html", player=id)
 
 
 if __name__ == "__main__":
