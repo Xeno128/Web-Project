67271fb48c87d4263b38bb86ec5feba8f1203424
יועד רביטל <yoad.ravitel@gmail.com>
2023-09-23 17:30:34 +0300

    updates

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import *
+from classes import *
+import pyrebase
+
+firebaseConfig = {
+  "apiKey": "AIzaSyCKRxeLwSq9giQXpo7pOQ-UaAy4i_UfFZQ",
+  "authDomain": "football-stats-7d1b2.firebaseapp.com",
+  "databaseURL": "https://football-stats-7d1b2-default-rtdb.firebaseio.com",
+  "projectId": "football-stats-7d1b2",
+  "storageBucket": "football-stats-7d1b2.appspot.com",
+  "messagingSenderId": "419599850111",
+  "appId": "1:419599850111:web:5724fff9a527ec7ac37d4a",
+  "measurementId": "G-N7L05EM3XD"
+};
+
+# initializing firebase database
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+# db.child("players").child(player.player_id).set(player.player_to_dict())
+# db.child("teams").child(player.team_name).set(player.team_to_dict())
 
 app = Flask(__name__)
 app.secret_key = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
@@ -20,9 +40,10 @@ def search():
     else:
         return render_template("search.html")    
 
-@app.route("/player-search?uid=<id>") 
+@app.route("/player-search/<id>") 
 def psearch(id):
-    return render_template("player.html", player=id)
+    player = Player(85, "https://media-1.api-sports.io/football/teams/160.png", "Paris Saint Germain", 278, "K. Mbapp\u00e9", "https://media-1.api-sports.io/football/players/125.png", 25, "178 cm", "75 kg", "Attacker", "France", 252, 163, "100 Out Of 153", 182, 12, 2)
+    return render_template("player.html", player=player)
 
 
 if __name__ == "__main__":
---
+++ classes.py
@@ -0,0 +1,36 @@
+class Team:
+    def __init__(self, team_id, team_logo, team_name):
+        self.team_id = team_id
+        self.team_logo = team_logo
+        self.team_name = team_name
+    
+    def team_to_dict(self):
+        return {"team_name": self.team_name, "team_id": self.team_id, "team_logo": self.team_logo}
+
+class Player(Team):
+    def __init__(self, team_id, team_logo, team_name, player_id, player_name, player_photo, player_age, player_height, player_weight, player_position, player_nationality, player_appearances, player_goals, player_pen_accuracy, player_assists, player_yellows, player_reds):
+        super().__init__(team_id, team_logo, team_name)
+        self.player_id = player_id
+        self.player_name = player_name
+        self.player_photo = player_photo
+        self.player_age = player_age
+        self.player_height = player_height
+        self.player_weight = player_weight
+        self.player_position = player_position
+        self.player_nationality = player_nationality
+        self.player_appearances = player_appearances
+        self.player_goals = player_goals
+        self.player_pen_accuracy = player_pen_accuracy
+        self.player_assists = player_assists
+        self.player_yellows = player_yellows
+        self.player_reds = player_reds
+        
+    def player_to_dict(self):
+        return {"player_id": self.player_id, "team_id": self.team_id, "player_name": self.player_name, "player_photo": self.player_photo, "player_age": self.player_age, "player_height": self.player_height, "player_weight": self.player_weight, "player_position": self.player_position, "player_nationality": self.player_nationality, "player_appearances": self.player_appearances, "player_goals": self.player_goals, "player_pen_accuracy": self.player_pen_accuracy, "player_assists": self.player_assists, "player_yellows": self.player_yellows, "player_reds": self.player_reds}
+
+class Users:
+    def __init__(self, username, password, location, photo):
+        self.username = username
+        self.password = password
+        self.location = location
+        self.photo = photo
